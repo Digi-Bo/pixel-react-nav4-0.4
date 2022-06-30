68958879fb0765f7426fcd0989081dc71291fe2f
Fanch <fanch.daniel@gmail.com>
2022-06-30 11:18:35 +0200

    Ajout d'un composant logo dans le header

--- screens/Portfolio.js
+++ screens/Portfolio.js
@@ -1,7 +1,17 @@
+
+/**
+ *  1. Création d'un composant Logo
+ *  2. Import du composant Logo
+ *  3. Intégration du composant logo dans navigationOption
+ */
+
+
 import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
 import { globalStyles } from "../style/AppStyles";
 
+/** 2 */
+import Logo from './Logo'
 
 const Portfolio = ({ navigation }) => {
   const handlePress = () => {
@@ -11,12 +21,22 @@ const Portfolio = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={globalStyles.h1}>Portofolio</Text>
-      <Text style={globalStyles.h2}>{navigation.getParam('name')}</Text>
-      <Text style={globalStyles.text}>{navigation.getParam('country')}</Text>
+      <Text style={globalStyles.h2}>{navigation.getParam("name")}</Text>
+      <Text style={globalStyles.text}>{navigation.getParam("country")}</Text>
       <Button title="Vers Photos" onPress={handlePress} />
     </View>
   );
 };
+
+
+
+/** 3 */
+Portfolio.navigationOptions = {
+  headerTitle:  () => <Logo/>, 
+  headerStyle:  {backgroundColor : "orangered"},
+};
+
+
 
 export default Portfolio;
 
@@ -29,6 +49,3 @@ const styles = StyleSheet.create({
   },
   text: {},
 });
-
-
-
